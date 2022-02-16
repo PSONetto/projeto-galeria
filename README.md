2d208b0cd73d5922abe0a10ce208926ce0da22f4
Plínio Netto <plinio.ice.pns@gmail.com>
2022-02-16 19:16:31 -0300

    Fix: adiciona README, LICENSE e novas imagens não-licenciadas, atualiza as dependências do package.json

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
 
-# Projeto Galeria
+# Projeto Galeria  
 
-## Integração de ***Bootstrap***, ***Webpack*** e ***jQuery***.<br>Galeria de fotos com filtro por cidade.
+[![Open in Visual Studio Code](https://open.vscode.dev/badges/open-in-vscode.svg)](https://open.vscode.dev/PSONetto/projeto-galeria) [![made-with-javascript](https://img.shields.io/badge/Made%20with-JavaScript-1f425f.svg)](https://www.javascript.com) [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)  
+
+Uma galeria de fotos simples que possui um filtro que mostrará fotos de uma cidade escolhida.  
+O projeto integra as tecnologias de ***Bootstrap***, ***jQuery*** e ***Webpack***, para propósitos de estudo.  
+Este projeto foi realizado como parte do [Curso Web Moderno Completo com JavaScript 2021 + Projetos](https://www.cod3r.com.br/courses/web-moderno).  
+
+> Projeto em construção  
+
+## Tecnologias  
+
+Projeto criado utilizando:  
+
++ ``JavaScript``  
++ ``HTML5``  
++ ``CSS3``  
++ ``SASS``  
++ ``jQuery``  
++ ``Bootstrap``  
++ ``Webpack``  
+
+## Créditos  
+
+[Cod3r: Cursos Online de Tecnologia](https://www.cod3r.com.br/)  
+[Pixabay](https://pixabay.com/)  
+[freeCodeCamp: How to write a good README file](https://www.freecodecamp.org/news/how-to-write-a-good-readme-file/)  
+[Alura: Como escrever um bom README](https://www.alura.com.br/artigos/escrever-bom-readme)  
+
+## Autor   
+
+| [<img src="https://avatars.githubusercontent.com/u/89879197?s=400&u=6e371fd0f1b5b2dc0df1e4212e34f600bc6a209b&v=4" width=115><br><sub>Plínio Netto</sub>](https://github.com/PSONetto) |  
+| :---: |  
+
+## Licença  
+
+[ISC](https://choosealicense.com/licenses/isc/)  
+[Imagens](https://pixabay.com/service/terms/#license)  
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,27 @@
-const modoDev = process.env.NODE_ENV !== 'production'
+// Imports de dependências e plugins
 const webpack = require('webpack')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
+// Variáveis e Constantes
+const devMode = process.env.NODE_ENV !== 'production'
+
+// Exports (CommonJS)
 module.exports = {
-    mode: modoDev ? 'development' : 'production',
-    entry: './src/index.js',
+    mode: devMode ? 'development' : 'production',
+    entry: {
+        main: './src/index.js'
+    },
     devServer: {
         contentBase: './build',
         port: 9000,
     },
     optimization: {
         minimizer: [
-            new UglifyJsPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: true
-            }),
-            new OptimizeCSSAssetsPlugin({})
+            new CssMinimizerPlugin(),
         ]
     },
     output: {
@@ -27,18 +29,23 @@ module.exports = {
         path: __dirname + '/build'
     },
     plugins: [
-        new MiniCssExtractPlugin({ filename: 'estilo.css' }),
+        new MiniCssExtractPlugin({ filename: 'style.css' }),
         new CopyWebpackPlugin([
             { context: 'src/', from: '**/*.html' },
             { context: 'src/', from: 'imgs/**/*' }
-        ])
+        ]),
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 12,
+            },
+        }),
     ],
     module: {
         rules: [{
             test: /\.s?[ac]ss$/,
             use: [
                 MiniCssExtractPlugin.loader,
-                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
                 'css-loader', // interpreta @import, url()...
                 'sass-loader',
             ]
