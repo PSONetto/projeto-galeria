5c96bf8814301e9c7cb7e3d267f503967fe8a1c3
Plínio Netto <plinio.ice.pns@gmail.com>
2022-02-19 19:02:27 -0300

    fix: posiciona as imagens e as formata

--- src/js/core/loadImgs.js
+++ src/js/core/loadImgs.js
@@ -2,21 +2,26 @@ import $ from 'jquery'
 
 import { onLoadHtmlSucces } from './includes'
 
+let barrier = false
+
 onLoadHtmlSucces(function() {
     $.ajax({
         type : 'GET',
         dataType : 'json',
         url: '/imgs/imgs.json',
         success : function(data) {
-            data.forEach(element => {
-                console.log(element.name)
-                let cityName = element.city
-                let imgName = element.name
-                let path = `imgs/${element.name}.jpg`
-                $('.img-box')
-                    .append(`<div class"col-12 col-md-6 col-lg-3"></div>`)
-                    .append(`<img src="${path}" alt="${imgName}" class="img-fluid mb-4" city="${cityName}">`)
-            })
+            if (barrier == false) {
+                data.forEach(element => {
+                    console.log(element.name)
+                    let cityName = element.city
+                    let imgName = element.name
+                    let path = `imgs/${element.name}.jpg`
+                    let divClass = 'col'
+                    $('.img-box')
+                        .append(`<div class="${divClass}"><img src="${path}" alt="${imgName}" class="img-fluid shadow bg-body rounded mb-4" style="height: 225px; width: 100%;" city="${cityName}"></div>`)
+                })
+                barrier = true
+            } 
         } 
     })
 })
