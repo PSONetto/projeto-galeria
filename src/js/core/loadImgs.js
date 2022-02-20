257edee000c57125140e6cf4975a4a60fd6793c4
Plínio Netto <plinio.ice.pns@gmail.com>
2022-02-20 18:11:19 -0300

    feat: implementa o layout dos botões

--- src/js/core/loadImgs.js
+++ src/js/core/loadImgs.js
@@ -2,8 +2,18 @@ import $ from 'jquery'
 
 import { onLoadHtmlSucces } from './includes'
 
-// Impede que as imagens sejam carregadas mais de uma vez
+// Impede que o JSON seja carregado mais de uma vez
 let barrier = false
+
+function capitalizeFirstLetter(string) {
+    if (string == 'nyc') {
+        return 'New York'
+    } else if (string == 'stpetersburg') {
+        return 'St. Petersburg'
+    } else {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+}
 
 onLoadHtmlSucces(function() {
     $.ajax({
@@ -11,6 +21,7 @@ onLoadHtmlSucces(function() {
         dataType: 'json',
         url: '/imgs/imgs.json',
         success: function(data) {
+            let citySet = new Set
             if (barrier == false) {
                 data.forEach(element => {
                     const cityName = element.city
@@ -24,10 +35,22 @@ onLoadHtmlSucces(function() {
                     const imgStyle = 'style="height: 225px; width: 100%;"'
                     const imgCity = `city="${cityName}"`
 
+                    const buttonName = capitalizeFirstLetter(cityName)
+                    citySet.add(buttonName)
+                    
+
                     $('#album')
                         .append(`<div ${divClass}><img ${imgSrc} ${imgAlt} ${imgClass} ${imgStyle} ${imgCity}></div>`)
+
+                    
                 })
                 barrier = true
+                console.log(citySet)
+
+                for (let item of citySet.values()) {
+                    $('#city-buttons')
+                        .append(`<button type="button" class="btn btn-outline-dark m-1">${item}</button>`)
+                }
             } 
         } 
     })
